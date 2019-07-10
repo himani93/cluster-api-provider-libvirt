a2bb0408c98426b55b737011b2229b2c11d07868
Himani Agrawal <himani.agrawal@go-jek.com>
2019-07-10 20:10:10 +0530

    Create machine via actuator

--- pkg/cloud/libvirt/actuators/machine/actuator.go
+++ pkg/cloud/libvirt/actuators/machine/actuator.go
@@ -2,14 +2,25 @@ package machine
 
 import (
 	"context"
+	"fmt"
+	"log"
 
+	"sigs.k8s.io/cluster-api-provider-libvirt/pkg/apis/libvirt/v1alpha1"
+	l "sigs.k8s.io/cluster-api-provider-libvirt/pkg/cloud/libvirt"
 	clusterv1 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/yaml"
 )
 
 const (
 	ProviderName = "libvirt"
 )
+
+// Add RBAC rules to access cluster-api resources
+//+kubebuilder:rbac:groups=cluster.k8s.io,resources=machines;machines/status,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=cluster.k8s.io,resources=machineClasses,verbs=get;list;watch
+//+kubebuilder:rbac:groups=cluster.k8s.io,resources=clusters;clusters/status,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=nodes;events,verbs=get;list;watch;create;update;patch;delete
 
 // Actuator is responsible for performing machine reconciliation
 type Actuator struct {
@@ -17,7 +28,23 @@ type Actuator struct {
 }
 
 func (a *Actuator) Create(ctx context.Context, cluster *clusterv1.Cluster, machine *clusterv1.Machine) error {
-	panic("not implemented")
+	if machine.Spec.ProviderSpec.Value == nil {
+		log.Printf("Machine Provider Spec not passed")
+		return fmt.Errorf("Machine Provider Spec not passed")
+	}
+
+	var providerSpec v1alpha1.LibvirtMachineProviderSpec
+	err := yaml.UnmarshalStrict(machine.Spec.ProviderSpec.Value.Raw, &providerSpec)
+	if err != nil {
+		log.Printf("Error unmarshalling machine provider spec: %+v", err)
+		return err
+	}
+
+	spec := providerSpec.Spec
+	log.Printf("Create machine actuator called for context %v, cluster %v, machine %v", ctx, cluster, machine)
+	l.CreateDomain(providerSpec.Name, spec.VCPU, uint(spec.MemoryInGB), spec.ImageURI, spec.UserDataURI)
+
+	return nil
 }
 
 func (a *Actuator) Delete(ctx context.Context, cluster *clusterv1.Cluster, machine *clusterv1.Machine) error {
@@ -29,7 +56,7 @@ func (a *Actuator) Update(ctx context.Context, cluster *clusterv1.Cluster, machi
 }
 
 func (a *Actuator) Exists(ctx context.Context, cluster *clusterv1.Cluster, machine *clusterv1.Machine) (bool, error) {
-	panic("not implemented")
+	return false, nil
 }
 
 func (a *Actuator) GetIP(cluster *clusterv1.Cluster, machine *clusterv1.Machine) (string, error) {
